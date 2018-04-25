6d9ce1fa4eef1d110592dc8c0dfe4c492f7b2b18
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-25 19:45:37 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,19 +1,22 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-      Show.maximum(:rating)
+    Show.maximum(:rating)
   end
 
-  def self.most_popular_show  
+  def self.most_popular_show
     Show.where(rating: highest_rating).first
   end
 
   def self.lowest_rating
+    Show.minimum(:rating)
   end
 
   def self.least_popular_show
+    Show.where(rating: lowest_rating).first
   end
 
   def self.ratings_sum
+    Show.sum(:rating)
   end
 
   def self.popular_shows
